dd579f28146606904e4170a979edd175dac7f93e
JeffDziad <jeffdziad@gmail.com>
2024-01-24 15:20:31 -0600

    Updates

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,20 +1,43 @@
-import { Grid } from "@mui/material";
+import { CardActions, CardContent, Typography, Card, Divider, Grid, IconButton } from "@mui/material";
 import "./Country.css";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import React, { useState } from "react";
 
 export default function Country(props) {
+    const [medals, setMedals] = useState(props.medals);
+
+    const addMedal = () => {
+        setMedals((prev) => prev+1);
+    }
+
+    const removeMedal = () => {
+        (medals <= 0) ? setMedals(0) : setMedals((prev) => prev-1);
+    }
+
     return (
-        <div className="country">
-            <Grid container spacing={2}>
-                <Grid item>
-                    {props.name}
-                </Grid>
-                <Grid item>
-                    Medals
-                </Grid>
-                <Grid item>
-                    Add
-                </Grid>
-            </Grid>
+        <div>
+            <Card sx={{boxShadow: 2, marginTop: '10px'}} className="countryCard">
+                <CardContent>
+                    <Typography sx={{fontSize: 16, marginBottom: 1, textDecoration: "underline"}}>
+                        {props.name}
+                    </Typography>
+                    <Typography sx={{fontSize: 20}} component="span" className="medal">
+                        {medals}
+                    </Typography>
+                </CardContent>
+                <Divider/>
+                <CardActions>
+                    <Grid container justifyContent="space-between">
+                        <Grid item>
+                            <IconButton color="error" onClick={removeMedal}><RemoveIcon/></IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton onClick={addMedal}><AddIcon/></IconButton>
+                        </Grid>
+                    </Grid>
+                </CardActions>
+            </Card>
         </div>
     );
 }
--- src/components/country_list/CountryList.jsx
+++ src/components/country_list/CountryList.jsx
@@ -1,22 +1,32 @@
-import { Button, Grid } from "@mui/material";
+import { Grid, IconButton, InputBase, Paper } from "@mui/material";
 import { useState } from "react";
+import "./CountryList.css";
 import AddIcon from '@mui/icons-material/Add';
 import Country from "../country/Country";
 
 export default function CountryList() {
-    const [countries, setCountries] = useState([]);
-    const [cid, setCid] = useState(0);
+    const [countryName, setCountryName] = useState("");
+    const [countries, setCountries] = useState([
+        {name: "United States of America", medals: 14},
+        {name: "Poland", medals: 4},
+        {name: "Sweden", medals: 8}
+    ]);
 
     function addCountry() {
-        setCountries(c => [...c, {name: "country" + cid}]);
-        setCid(c => c+1);
+        if(countryName.length > 0 && countryName) {
+            setCountries(c => [...c, {name: countryName}]);
+            setCountryName("");
+        }
     }
 
     return (
         <Grid>
+            <Paper component="form" onSubmit={e => e.preventDefault()} className="addCountryInput">
+                <InputBase value={countryName} onChange={event => {setCountryName(event.target.value)}} placeholder="Country Name" sx={{ p: '10px' }}></InputBase>
+                <IconButton type="button" onClick={addCountry}><AddIcon/></IconButton>
+            </Paper>
             {(countries.length <= 0) ? <p style={{textAlign: 'center'}}>Your list is empty!</p> : ""}
-            {countries.map((country) => <Country name={country.name}/>)}
-            <Button onClick={addCountry} variant="text"><AddIcon/>Add Country</Button>
+            {countries.map((country) => <Country key={country.name} name={country.name} medals={country.medals}/>)}
         </Grid>
     );
 }
